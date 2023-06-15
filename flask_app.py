3dc36ccc9295426b0b7f41d25271b8f51eec9e61
shlomi cohen <100487390+shlomi888c@users.noreply.github.com>
2023-06-15 20:59:58 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -23,7 +23,7 @@ def search():
   s.close()
 
   #consul_ip = os.environ.get('ip_consul')
-  consul_url = f"http://35.172.212.93:8500/v1/agent/service/register"
+  consul_url = f"http://54.226.128.182:8500/v1/agent/service/register"
   data = {
        "ID": "flask-app" + flask_app_ip,
        "Name": "flask-app" + flask_app_ip,
